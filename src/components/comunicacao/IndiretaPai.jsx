331a7c0b43be48728946e0449ed09714a21b009f
Victor <victorjuanrazer@gmail.com>
2022-10-28 16:44:01 -0300

    comunication indirect 1/2

---
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import IndiretaFilho from "./IndiretaFilho";
+
+export default (props) => {
+  // nome idade nerd
+
+  return (
+    <div>
+      Pai <IndiretaFilho></IndiretaFilho>
+    </div>
+  );
+};
